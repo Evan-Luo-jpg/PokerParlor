9f91c75d3dd3a18b851f7e9d064cc48b5daa887d
root <root@Headlessbear.localdomain>
2025-07-04 21:37:16 -0600

    Readme Done

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     Game game(10, 20); // Small blind = 10, Big blind = 20
 
     // Add players
-    game.addPlayer(Player(0, 100, true)); // Just for testing right now
+    game.addPlayer(Player(0, 100, false)); // Just for testing right now
     game.addPlayer(Player(1, 100, true)); // Bot player
     game.addPlayer(Player(2, 100, true)); // Bot player
     game.addPlayer(Player(3, 100, true)); // Bot player
